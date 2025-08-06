8ee12f4901d88cb80ce7003f8c4e7c4036e60530
Felipe Almeida <felipeortalmeida@gmail.com>
2025-08-05 23:53:53 -0300

    feat: create some basic UI & styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import Rating from './components/Rating'
 
 const App = () => {
-  return (
-    <>
-      <h1>Hello, Stranger</h1>
-      <Rating />
-    </>
-  )
+  return <Rating />
 }
 
 export default App
--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -1,3 +1,14 @@
-const Rating = () => <p>Rate Your Experience</p>
+const Rating = () => (
+  <div className="rating">
+    <h2>Rate Your Experience</h2>
+    <div className="stars">
+      {[1, 2, 3, 4, 5].map((star) => (
+        <span key={star} className="star">
+          {'\u2605'}
+        </span>
+      ))}
+    </div>
+  </div>
+)
 
 export default Rating
