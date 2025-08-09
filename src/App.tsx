235d5aef4b6b88e7eb9b631946beeb6e1475bb8f
Felipe Almeida <felipeortalmeida@gmail.com>
2025-08-09 13:18:17 -0300

    fix: use another approach for handling 100% height designs on mobile (wider browser support)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
+import { useEffect } from 'react'
 import Rating from './components/Rating'
+import './index.css'
 
 const App = () => {
+  // Using native VH is strange on mobile, so we calculate a new VH unit here
+  // https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
+  useEffect(() => {
+    const fixVhUnit = () => {
+      window.document.documentElement.style.setProperty('--vh', `${innerHeight * 0.01}px`)
+    }
+    fixVhUnit()
+    addEventListener('resize', fixVhUnit)
+    return () => {
+      removeEventListener('resize', fixVhUnit)
+    }
+  }, [])
+
   return <Rating />
 }
 
