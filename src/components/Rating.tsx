47a606450aceb9cac17e06f42b77d5d2eec5c17c
Felipe Almeida <felipeortalmeida@gmail.com>
2025-08-09 13:21:34 -0300

    refactor: Rating component stylesheet

--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -1,9 +1,11 @@
+import styles from './Rating.module.css'
+
 const Rating = () => (
-  <div className="rating">
+  <div className={styles.rating}>
     <h2>Rate Your Experience</h2>
-    <div className="stars">
+    <div className={styles.stars}>
       {[1, 2, 3, 4, 5].map((star) => (
-        <span key={star} className="star">
+        <span key={star} className={styles.star}>
           {'\u2605'}
         </span>
       ))}
