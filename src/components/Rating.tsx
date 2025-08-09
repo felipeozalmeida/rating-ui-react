18b8e35b6c5458b43673cd447843ee646ea549ee
Felipe Almeida <felipeortalmeida@gmail.com>
2025-08-09 13:47:05 -0300

    feat: reflect chosen option in the UI

--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -1,16 +1,26 @@
+import { useState } from 'react'
+import clsx from 'clsx'
 import styles from './Rating.module.css'
 
-const Rating = () => (
-  <div className={styles.rating}>
-    <h2>Rate Your Experience</h2>
-    <div className={styles.stars}>
-      {[1, 2, 3, 4, 5].map((star) => (
-        <span key={star} className={styles.star}>
-          {'\u2605'}
-        </span>
-      ))}
+const Rating = () => {
+  const [rating, setRating] = useState(0)
+  return (
+    <div className={styles.rating}>
+      <p>Rate Your Experience</p>
+      <div className={styles.stars}>
+        {[1, 2, 3, 4, 5].map((star) => (
+          <button
+            key={star}
+            className={clsx(styles.star, { [styles['star--active']]: star === rating })}
+            type="button"
+            onClick={() => setRating((prevRating) => (prevRating === star ? 0 : star))}
+          >
+            {'\u2605'}
+          </button>
+        ))}
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Rating
