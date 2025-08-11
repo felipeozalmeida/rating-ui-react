81e0d05e8e74e532c259cf65543bfa9a03625ba1
Felipe Almeida <felipeortalmeida@gmail.com>
2025-08-10 23:47:06 -0300

    refactor: components

---
+++ src/components/Rating/index.tsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import styles from './styles.module.scss'
+import Star from '../Star'
+
+const defaultTitle = 'Rate Your Experience'
+const defaultMessages = ['Terrible', 'Poor', 'Fair', 'Good', 'Excellent']
+
+const Rating = ({ title = defaultTitle, messages = defaultMessages }) => {
+  const [currentValue, setCurrentValue] = useState(0)
+  const [hoveredValue, setHoveredValue] = useState(0)
+
+  const feedback = messages[currentValue - 1] || defaultMessages[currentValue - 1]
+
+  return (
+    <div className={styles.rating}>
+      <p className={styles.title}>{title || defaultTitle}</p>
+      <div className={styles.stars}>
+        {[1, 2, 3, 4, 5].map((value) => (
+          <Star
+            key={value}
+            value={value}
+            currentValue={currentValue}
+            hoveredValue={hoveredValue}
+            onHover={setHoveredValue}
+            onClick={setCurrentValue}
+          />
+        ))}
+      </div>
+      {feedback && <p className={styles.feedback}>{feedback}</p>}
+    </div>
+  )
+}
+
+export default Rating
---
+++ src/components/Star/index.tsx
@@ -0,0 +1,32 @@
+import clsx from 'clsx'
+import styles from './styles.module.scss'
+
+const Star = ({
+  value,
+  currentValue,
+  hoveredValue,
+  onHover,
+  onClick,
+}: {
+  value: number
+  currentValue: number
+  hoveredValue: number
+  onHover: (star: number) => void
+  onClick: (star: number) => void
+}) => {
+  return (
+    <button
+      type="button"
+      className={clsx(styles.star, {
+        [styles['star--active']]: value <= currentValue || value <= hoveredValue,
+      })}
+      onMouseEnter={() => onHover(value)}
+      onMouseLeave={() => onHover(0)}
+      onClick={() => onClick(currentValue === value ? 0 : value)}
+    >
+      {'\u2605'}
+    </button>
+  )
+}
+
+export default Star
